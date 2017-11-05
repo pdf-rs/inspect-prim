8bf788cc5c0a1035060e39815740d9abbcab911c
Erlend Langseth <3rlendhl@gmail.com>
2017-11-05 16:35:26 +0100

    (not sure), changing how things work

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ extern crate imgui_gfx_renderer;
 
 extern crate pdf;
 
+pub mod search;
+use search::*;
+
 use imgui::*;
 use std::str;
 
@@ -15,14 +18,67 @@ use object::*;
 use backend::*;
 use primitive::*;
 
+use std::cell::RefCell;
+
 mod support_gfx;
 
 const CLEAR_COLOR: [f32; 4] = [0.2, 0.2, 0.3, 1.0];
 
+fn main() {
+    let backend = Vec::<u8>::open("files/jpeg.pdf").unwrap();
+    let (xref_tab, trailer) = backend.read_xref_table_and_trailer().unwrap();
+
+    let mut search_paths = RefCell::new(Vec::new());
+
+    support_gfx::run("hello_gfx.rs".to_owned(), CLEAR_COLOR, |ui| {
+        let inspector = Inspector::new(ui, |x| backend.resolve(&xref_tab, x) );
+        ui.window(im_str!("Inspect PDF"))
+            .size((300.0, 100.0), ImGuiSetCond_FirstUseEver)
+            .build(|| {
+                inspector.draw(ui, &trailer);
+            });
+
+        // Search window
+        ui.window(im_str!("Search PDF"))
+            .size((300.0, 100.0), ImGuiSetCond_FirstUseEver)
+            .build(|| {
+                let mut search_term = ImString::with_capacity(20);
+                if ui.input_text(im_str!("Search term"), &mut search_term).enter_returns_true(true).build() {
+                    // Start search!
+                    let mut search_paths = &mut *search_paths.borrow_mut();
+                    *search_paths = inspector.search_key(&trailer.clone().into(), search_term.to_str());
+
+                    println!("Paths: {:?}", search_paths);
+                }
+                for path in &*search_paths.borrow() {
+                    ui.text(im_str!("{}", path_to_string(&path)));
+                }
+            });
+
+        true
+    });
+}
+
+fn path_to_string(path: &SearchPath) -> String {
+    let mut result: String = String::new();
+    for elem in path.iter() {
+        match *elem {
+            PathElem::DictElem {ref key} => {
+                result += "->";
+                result += &key
+            }
+            PathElem::ArrayElem {index} => {
+                result += &format!("[{}]", index);
+            }
+        }
+    }
+    result
+}
+
+
 struct Inspector<'a, 'b: 'a, R: Resolve> {
     ui: &'a Ui<'b>,
     resolve: R ,
-    unique_id: i32,
 }
 
 impl<'a, 'b, R: Resolve> Inspector<'a, 'b, R> {
@@ -30,26 +86,21 @@ impl<'a, 'b, R: Resolve> Inspector<'a, 'b, R> {
         Inspector {
             ui: ui,
             resolve: resolve,
-            unique_id: 0,
         }
     }
-    fn new_id(&mut self) -> i32 {
-        self.unique_id += 1;
-        self.unique_id
-    }
-    fn draw(&mut self, ui: &Ui, root: &Dictionary) {
+    pub fn draw(&self, ui: &Ui, root: &Dictionary) {
         ui.text(im_str!("PDF file"));
         ui.separator();
         self.view_dict(root);
     }
 
-    fn view_primitive(&mut self, prim: &Primitive) {
+    pub fn view_primitive(&self, prim: &Primitive) {
         match *prim {
             Primitive::Null => {},
             Primitive::Integer (x) => self.ui.text(im_str!("{}", x)),
             Primitive::Number (x) => self.ui.text(im_str!("{}", x)),
             Primitive::Boolean (x) => self.ui.text(im_str!("{}", x)),
-            Primitive::String (ref x) => self.ui.text(im_str!("\"{}\"", x.as_str().unwrap())),
+            Primitive::String (ref x) => self.ui.text(im_str!("\"{}\"", x.as_str().unwrap_or("<indiscernible string>"))),
             Primitive::Stream (ref x) => {
                 self.attr("Data", &PdfString::new(x.data.clone()).into(), 0);
                 self.attr("Info", &x.info.clone().into(), 1);
@@ -65,44 +116,30 @@ impl<'a, 'b, R: Resolve> Inspector<'a, 'b, R> {
             Primitive::Reference (ref x) => {
                 match self.resolve.resolve(*x) {
                     Ok(primitive) => {
-                        self.attr("", &primitive, 0);
+                        self.attr(&format!("Ref[{}, {}]", x.id, x.gen), &primitive, 0);
                     }
-                    Err(_) => {im_str!("<error resolvind object>");},
+                    Err(_) => {im_str!("<error resolving object>");},
                 }
             }
             Primitive::Name (ref x) => self.ui.text(im_str!("/{}", x))
         };
     }
 
-    fn view_dict(&mut self, dict: &Dictionary) {
+    pub fn view_dict(&self, dict: &Dictionary) {
         let mut id = 0;
         for (key, val) in dict.iter() {
             self.attr(key, val, id);
             id += 1;
         }
+        if dict.len() == 0 {
+            self.ui.text(im_str!("<No entries in dictionary>"));
+        }
     }
 
     /// Note: the point with `id` is just that ImGui needs some unique string identifier for each
     /// tree node on the same level.
-    fn attr(&mut self, name: &str, val: &Primitive, id: i32) {
+    pub fn attr(&self, name: &str, val: &Primitive, id: i32) {
         let name = im_str!("{} <{}>", name, val.get_debug_name());
         self.ui.tree_node(im_str!("{}", id)).label(name).build(|| self.view_primitive(val));
     }
-}
-
-fn main() {
-    let backend = Vec::<u8>::open("files/libreoffice.pdf").unwrap();
-    let (xref_tab, trailer) = backend.read_xref_table_and_trailer().unwrap();
-
-
-    support_gfx::run("hello_gfx.rs".to_owned(), CLEAR_COLOR, |ui| {
-        let mut inspector = Inspector::new(ui, |x| backend.resolve(&xref_tab, x) );
-        ui.window(im_str!("Inspect PDF"))
-            .size((300.0, 100.0), ImGuiSetCond_FirstUseEver)
-            .build(|| {
-                inspector.draw(ui, &trailer);
-            });
-        // ui.show_style_editor(ui.imgui().style_mut());
-        true
-    });
 }
---
+++ src/search.rs
@@ -0,0 +1,116 @@
+use pdf::*;
+use object::*;
+use backend::*;
+use primitive::*;
+
+use std::collections::{VecDeque, HashSet};
+use std::collections::vec_deque;
+use std::str::FromStr;
+
+use Inspector;
+
+
+#[derive(Debug)]
+pub enum PathElem {
+    DictElem {key: String},
+    ArrayElem {index: i32},
+}
+impl PathElem {
+    pub fn from_dict_key(key: &str) -> PathElem {
+        PathElem::DictElem {key: String::from_str(key).unwrap()}
+    }
+    pub fn from_array_index(index: i32) -> PathElem {
+        PathElem::ArrayElem {index: index}
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct SearchPath {
+    path: VecDeque<PathElem>,
+}
+impl SearchPath {
+    pub fn new(start: PathElem) -> SearchPath {
+        let mut new_path = SearchPath::default();
+        new_path.path.push_back(start);
+        new_path
+    }
+    /// Add element to the left-hand side, or the beginning, of the path
+    pub fn add_to_start(&mut self, elem: PathElem) {
+        self.path.push_front(elem);
+    }
+    pub fn iter(&self) -> vec_deque::Iter<PathElem> {
+        self.path.iter()
+    }
+}
+
+
+/// Used internally to store information about where we have been
+struct SearchAlg<'a, R: Resolve + 'a> {
+    // Only follow each reference once
+    blacklist: HashSet<PlainRef>,
+    resolve: &'a R,
+}
+impl<'a, R: Resolve> SearchAlg<'a, R> {
+    fn new(r: &'a R) -> SearchAlg<'a, R> {
+        SearchAlg {
+            blacklist: HashSet::new(),
+            resolve: r
+        }
+    }
+    fn search_key(&mut self, node: &Primitive, search_key: &str) -> Vec<SearchPath> {
+        match *node {
+            Primitive::Stream (ref stream) => {
+                self.search_key(&stream.info.clone().into(), search_key)
+            },
+            Primitive::Dictionary (ref dict) => {
+                let mut result = Vec::new();
+                for (key, node) in dict.iter() {
+                    if key == search_key {
+                        result.push( SearchPath::new(PathElem::from_dict_key(key)) );
+                    }
+
+
+                    // Traverse the child nodes, adding this node the partial paths that result
+                    let mut subresults = self.search_key(node, search_key);
+                    for path in &mut subresults {
+                        path.add_to_start(PathElem::from_dict_key(key));
+                    }
+                    result.append(&mut subresults);
+                }
+                result
+            },
+            Primitive::Array (ref arr) => {
+                let mut result = Vec::new();
+                for node in arr {
+                    let mut subresults = self.search_key(node, search_key);
+                    for (i, path) in &mut subresults.iter_mut().enumerate() {
+                        path.add_to_start(PathElem::from_array_index(i as i32));
+                    }
+                    result.append(&mut subresults);
+                }
+                result
+            },
+            Primitive::Reference (reference) => {
+                if !self.blacklist.contains(&reference) {
+                    self.blacklist.insert(reference);
+                    let prim = match self.resolve.resolve(reference) {
+                        Ok(prim) => prim,
+                        Err(_) => return Vec::new(),
+                    };
+                    self.search_key(&prim, search_key)
+                } else {
+                    Vec::new()
+                }
+            },
+            _ => Vec::new(),
+        }
+    }
+}
+
+impl<'a, 'b, R: Resolve> Inspector<'a, 'b, R> {
+
+    pub fn search_key(&self, node: &Primitive, search_key: &str) -> Vec<SearchPath> {
+        let mut alg = SearchAlg::new(&self.resolve);
+        alg.search_key(node, search_key)
+    }
+}
